16c2da622f9f1bd4ee7fd32b098f93f985dc39b2
PYOJAN <pintuprajapati4@gmail.com>
2022-08-03 16:16:01 +0530

    UI for certificate separated based on plateform

--- src/components/Forms/FormRight.jsx
+++ src/components/Forms/FormRight.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Checkbox from "../shared/CheckboxGroup/Checkbox";
 import SignatureConfigField from "../shared/SignatureConfigField/SignatureConfigField";
 import { useDataLayerValue } from "../Contexts/DataLayer";
@@ -8,11 +8,17 @@ import AppServiceHandle from "../AppServiceHadle/AppServiceHandle";
 
 import "./FormRight.css";
 import InputGroup from "../shared/InputGroup/InputGroup";
-import Input from "../shared/Input/Input";
 
 const FormRight = () => {
   const [initData, dispatch] = useDataLayerValue();
   const { enableLTV, enableTimestamp, isVisible, certs } = initData;
+  const [linuxCertValue, setLinuxCertValue] = useState(certs);
+
+  // console.log({ initData });
+
+  useEffect(() => {
+    setLinuxCertValue(certs);
+  }, [certs]);
 
   const handleChangeValue = ({ name, value }) => {
     dispatch({
@@ -22,18 +28,59 @@ const FormRight = () => {
     });
   };
 
+  const handleCertChange = (e) => {
+    e.preventDefault();
+
+    setLinuxCertValue({
+      name: certs.name,
+      sn: e.target.value,
+    });
+  };
+
+  const saveCertvalue = () => {
+    dispatch({
+      type: actionTypes.UPDATE_SINGLE,
+      field: "certs",
+      value: linuxCertValue,
+    });
+  };
+  const saveToDatabase = (e) => {
+    if (e.key === "Enter" || e.key === "numEnter") {
+      saveCertvalue();
+    }
+  };
+
+  // Inline css
+  const inputStyle = {
+    backgroundColor: "rgb(59, 66, 82)",
+    border: "1px solid transparent",
+    color: "#B2C8DF",
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    width: "30px!important",
+  };
+
   return (
     <>
       <form className="w-100 pb-2 p-1 pt-1">
-        {certs?.platform === "linux" ? (
+        {window.navigator?.platform.includes("Linux") ? (
           <SignatureConfigField>
             <InputGroup classes="w-100" feildLabel="Certificate Serial Number">
-              <Input
+              <input
                 type="text"
-                name="certSn"
-                inputValue={"cert"}
-                placeholder="Your Custome Test"
-                // onHandleChangevalue={handleCoordAndSize}
+                className="form-control form-control-sm px-2"
+                style={inputStyle}
+                value={linuxCertValue.name === null ? "" : linuxCertValue.name}
+                disabled={true}
+              />
+              <input
+                type="text"
+                className="form-control form-control-sm px-2"
+                style={inputStyle}
+                value={linuxCertValue.sn === null ? "" : linuxCertValue.sn}
+                onChange={handleCertChange}
+                onKeyDown={saveToDatabase}
               />
             </InputGroup>
           </SignatureConfigField>
